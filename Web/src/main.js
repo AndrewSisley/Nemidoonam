d9cb15bd28344958ee82c2a371de563214ef98cb
Andrew Sisley <a.j.sisley@hotmail.com>
2019-12-30 00:17:56 +0100

    Add basic style system

--- Web/src/main.js
+++ Web/src/main.js
@@ -1,5 +1,7 @@
 import { start_app } from 'nemidoonam';
 
 (() => {
-    document.getElementById("container").innerHTML = start_app().html;
+    const app = start_app();
+    document.getElementById("component-styles").innerHTML = app.css;
+    document.getElementById("container").innerHTML = app.html;
 })();

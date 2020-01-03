1616d62b5b2d6ddfd8d513250031024509ec4712
Andrew Sisley <a.j.sisley@hotmail.com>
2020-01-03 19:53:49 +0100

    Add home and alphabet pages
    Includes basic page system

---
+++ Web/src/pages/alphabet.rs
@@ -0,0 +1,27 @@
+use wacm::Component;
+use crate::localization::{ label::Label, label_definition::LabelDefinition };
+
+pub static TITLE: LabelDefinition = LabelDefinition {
+    id: 3,
+    english: Label {
+        display_text: "Alphabet",
+    },
+    farsi: Label {
+        display_text: "Alaf be",
+    },
+    swedish: Label {
+        display_text: "Alfabet",
+    },
+};
+
+pub fn get_page() -> Component {
+    return Component {
+        css: format!(
+            ""
+        ),
+        html: format!(
+            "<h2>{title}</h2>",
+            title = TITLE.get_display_text(),
+        ),
+    }
+}

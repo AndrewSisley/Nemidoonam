a313e78f09251737151819d7875ee59df3e52e24
Andrew Sisley <a.j.sisley@hotmail.com>
2020-02-03 17:48:49 +0000

    Add get_target_text function to LabelDefinition

--- Web/src/localization/label_definition.rs
+++ Web/src/localization/label_definition.rs
@@ -1,5 +1,5 @@
 use super::label::Label;
-use crate::repos::display_language;
+use crate::repos::{ display_language, target_language };
 
 pub struct LabelDefinition<'a> {
     pub english: Label<'a>,
@@ -10,6 +10,17 @@ pub struct LabelDefinition<'a> {
 impl<'a> LabelDefinition<'a> {
     pub fn get_display_text(&self) -> &'a str {
         return match display_language::get() {
+            1 => self.english.display_text,
+            2 => self.farsi.display_text,
+            3 => self.swedish.display_text,
+            _ => &"",
+        }
+    }
+}
+
+impl<'a> LabelDefinition<'a> {
+    pub fn get_target_text(&self) -> &'a str {
+        return match target_language::get() {
             1 => self.english.display_text,
             2 => self.farsi.display_text,
             3 => self.swedish.display_text,

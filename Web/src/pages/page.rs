2d18f40216f20bc4392a744bf818b94c2d57726f
Andrew Sisley <a.j.sisley@hotmail.com>
2020-02-04 15:36:02 +0000

    Add page section with styles

--- Web/src/pages/page.rs
+++ Web/src/pages/page.rs
@@ -5,16 +5,36 @@ use wacm::Component;
 use super::{ home, alphabet };
 use crate::repos;
 
+const PAGE_CLASS_NAME: &'static str = "page-body";
+
 pub fn get_current_page() -> Component {
     let current_page_id = repos::current_page::get();
     return get_page(current_page_id);
 }
 
 fn get_page(page_id: i32) -> Component {
-    match page_id {
+    let page_content = match page_id {
         1 => home::get_page(),
         2 => alphabet::get_page(),
         _ => home::get_page(),
+    };
+
+    Component {
+        css: format!(
+            ".{page_class_name} {{
+            margin: 0 0 0 100px;
+            }}
+            {page_content}",
+            page_class_name = PAGE_CLASS_NAME,
+            page_content = page_content.css
+        ),
+        html: format!(
+            "<section class='{page_class_name}'>
+            {page_content}
+            </section>",
+            page_class_name = PAGE_CLASS_NAME,
+            page_content = page_content.html
+        )
     }
 }
 

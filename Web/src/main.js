79324b6c52b35cec6c1766c5b37ad83bda17d0f6
Andrew Sisley <a.j.sisley@hotmail.com>
2020-02-05 11:13:44 +0000

    Refactor ui refresh logic

--- Web/src/main.js
+++ Web/src/main.js
@@ -8,11 +8,13 @@ import {
     set_page
 } from 'nemidoonam';
 
-(() => {
+let refreshUI = () => {
     const app = start_app();
     document.getElementById("component-styles").innerHTML = app.css;
     document.getElementById("container").innerHTML = app.html;
-})();
+};
+
+refreshUI();
 
 // Note should be handled as part of nemidoonam.package.json in /pkg, however I cant yet find a way to get wasm-pack to do this for me
 window.nemidoonam = {
@@ -44,8 +46,6 @@ window.nemidoonam = {
     set_page: (id) => {
         set_page(id);
         // this should only set the relevant elements, and be done a little less explicitly perhaps
-        const app = start_app();
-        document.getElementById("component-styles").innerHTML = app.css;
-        document.getElementById("container").innerHTML = app.html;
+        refreshUI();
     },
 };

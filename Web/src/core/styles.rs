6fcbf8e7ca5824e0696c266394682a80623f5ccc
Andrew Sisley <a.j.sisley@hotmail.com>
2020-02-05 11:16:42 +0000

    Introduce shared display and target text css classes

--- Web/src/core/styles.rs
+++ Web/src/core/styles.rs
@@ -1,8 +1,23 @@
+use crate::repos::{ text_direction };
+
 pub fn get_shared_css() -> String {
-    "h1 {
-    font-size: 3em;
-    }
-    h2 {
-    font-size: 2.5em;
-    }".to_string()
+    let display_text_direction = text_direction::get_display();
+    let target_text_direction = text_direction::get_target();
+
+    format!(
+        "h1 {{
+        font-size: 3em;
+        }}
+        h2 {{
+        font-size: 2.5em;
+        }}
+        .display-text {{
+        direction: {display_text_direction}
+        }}
+        .target-text {{
+        direction: {target_text_direction}
+        }}",
+        display_text_direction = display_text_direction,
+        target_text_direction = target_text_direction,
+    )
 }
